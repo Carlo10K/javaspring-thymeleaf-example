1454c89794f4fc2c517ff991fecc27dd6015ea22
Carlo10K <carlosortegadiaz50@gmail.com>
2023-01-04 17:59:06 -0600

    first commit

---
+++ src/test/java/com/bolsadeidas/springboot/web/app/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolsadeidas.springboot.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
