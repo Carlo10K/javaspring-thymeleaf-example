bdcf4799462eccfdf28278ab7b5b54bdaacf40fa
Carlo10K <carlosortegadiaz50@gmail.com>
2023-05-04 12:07:37 -0600

    enviar parametros mixtos

--- src/main/java/com/bolsadeidas/springboot/web/app/controllers/EjemploParamsController.java
+++ src/main/java/com/bolsadeidas/springboot/web/app/controllers/EjemploParamsController.java
@@ -1,5 +1,6 @@
 package com.bolsadeidas.springboot.web.app.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,26 @@ public class EjemploParamsController {
     @GetMapping("/string")
     public String param(@RequestParam(name = "txt", required = false, defaultValue = "default") String txt, Model model){
         model.addAttribute("result","El texto enviado es: "+txt);
+        return "params/ver";
+    }
+
+    @GetMapping("/mix-params")
+    public String param(@RequestParam String saludo, @RequestParam Integer numero, Model model){
+        model.addAttribute("result","El saludo es : '"+saludo +"' y el numero es: '"+numero+"'");
+        return "params/ver";
+    }
+
+    @GetMapping("/mix-params-request")
+    public String param(HttpServletRequest request, Model model){
+        String saludo = request.getParameter("saludo");
+        Integer numero = null;
+        try {
+            numero = Integer.parseInt(request.getParameter("numero"));
+        }catch (NumberFormatException e){
+            numero=0;
+        }
+
+        model.addAttribute("result","El saludo es : '"+saludo +"' y el numero es: '"+numero+"'");
         return "params/ver";
     }
 }
